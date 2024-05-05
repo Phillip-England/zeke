8a73b24b1c467cf1de28d2885ddfa866ef49236e
Phillip England <phillipengland@jacex-2.local>
2024-05-05 04:44:46 -0500

    added .gitignore and removed target

--- target/debug/deps/rust_http_quickstart-30294ee66f014c7c.d
+++
@@ -1,9 +0,0 @@
-/Users/phillipengland/pro/rust-http-quickstart/target/debug/deps/librust_http_quickstart-30294ee66f014c7c.rmeta: src/main.rs src/http/mod.rs src/http/request.rs src/http/response.rs src/http/pool.rs
-
-/Users/phillipengland/pro/rust-http-quickstart/target/debug/deps/rust_http_quickstart-30294ee66f014c7c.d: src/main.rs src/http/mod.rs src/http/request.rs src/http/response.rs src/http/pool.rs
-
-src/main.rs:
-src/http/mod.rs:
-src/http/request.rs:
-src/http/response.rs:
-src/http/pool.rs:
--- target/debug/deps/rust_http_quickstart-4ad04e79356175d3.d
+++
@@ -1,9 +0,0 @@
-/Users/phillipengland/pro/rust-http-quickstart/target/debug/deps/rust_http_quickstart-4ad04e79356175d3: src/main.rs src/http/mod.rs src/http/request.rs src/http/response.rs src/http/pool.rs
-
-/Users/phillipengland/pro/rust-http-quickstart/target/debug/deps/rust_http_quickstart-4ad04e79356175d3.d: src/main.rs src/http/mod.rs src/http/request.rs src/http/response.rs src/http/pool.rs
-
-src/main.rs:
-src/http/mod.rs:
-src/http/request.rs:
-src/http/response.rs:
-src/http/pool.rs:
